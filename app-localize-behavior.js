57a7bad9a77ce26065f02ba3ba259797e44eeb56
Alexander Marks <aomarks@google.com>
2018-01-24 15:02:56 -0800

    auto-converted by polymer-modulizer

--- app-localize-behavior.js
+++ app-localize-behavior.js
@@ -1,18 +1,6 @@
-<!--
-@license
-Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-
-<link rel="import" href="../polymer/polymer.html">
-<link rel="import" href="../iron-ajax/iron-ajax.html">
-<script src="../intl-messageformat/dist/intl-messageformat.min.js"></script>
-
-<script>
+import '../polymer/polymer.js';
+import '../iron-ajax/iron-ajax.js';
+import '../../intl-messageformat/dist/intl-messageformat.min.js';
 
 /**
 * `Polymer.AppLocalizeBehavior` wraps the [format.js](http://formatjs.io/) library to
@@ -119,6 +107,15 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 *
 * @demo demo/index.html
 * @polymerBehavior Polymer.AppLocalizeBehavior
+*/
+/**
+@license
+Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 Polymer.AppLocalizeBehavior = {
   /**
@@ -330,6 +327,4 @@ Polymer.AppLocalizeBehavior = {
       proto['__localizationCache'] = {requests: {}, messages: {}, ajax: null};
     }
   }
-}
-
-</script>
+};
---
+++ demo/common-styles.js
@@ -0,0 +1,55 @@
+import '../../polymer/polymer.js';
+import '../../paper-styles/shadow.js';
+import '../../paper-styles/typography.js';
+import '../../iron-demo-helpers/demo-snippet.js';
+const $_documentContainer = document.createElement('div');
+$_documentContainer.setAttribute('style', 'display: none;');
+
+$_documentContainer.innerHTML = `<dom-module id="common-styles">
+  <template>
+    <style include="demo-snippet">
+      :host {
+        @apply --shadow-none;
+        @apply --paper-font-common-base;
+        display: block;
+        max-width: 600px;
+        margin: 40px auto;
+      }
+
+      .snippet {
+        @apply --shadow-elevation-2dp;
+      }
+
+      paper-toggle-button {
+        display: inline-block;
+      }
+
+      .lang {
+        text-align: center;
+        font-size: 20px;
+      }
+
+      code {
+        display: block;
+      }
+
+      .demo div {
+        @apply --paper-font-body1;
+        line-height: 1.5;
+      }
+    </style>
+  </template>
+</dom-module>`;
+
+document.head.appendChild($_documentContainer);
+
+/**
+@license
+Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+;
